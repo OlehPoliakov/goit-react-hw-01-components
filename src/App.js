cbc377ba2abe4686c15d246d009701c847fe0c41
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-09-14 02:22:15 +0300

    createProfile

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import Wrapper from './components/Wrapper';
+import Profile from './components/Profile';
+import user from './data/user.json';
+
+export default function App() {
+  return (
+    // <Wrapper>
+      <Profile
+        key={user.id}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    /* </Wrapper> */
+  );
+}
+
